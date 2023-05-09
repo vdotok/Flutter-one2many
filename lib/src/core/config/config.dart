1b7dca8d3d5245a8f3d20930179f4973ff25d166
488taimoor <488taimoor@gmail.com>
2023-05-09 13:48:50 +0500

    porjectId changed

--- lib/src/core/config/config.dart
+++ lib/src/core/config/config.dart
@@ -2,7 +2,7 @@ var URL = "https://q-tenant.vdotok.dev";
 const version = "/API/v0/";
 
 const String auth_token = "3d9686b635b15b5bc2d19800407609fa";
-const String project_id ="115G1WZI";
+const String project_id = "1RN1RP";
 // const String project_id = "115G1WZI";
 
 // var URL = "https://d-tenant.vdotok.dev";
